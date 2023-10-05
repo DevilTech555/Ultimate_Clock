94ba96fa9a1c5f9928cc3c6d0f54323ba54a0885
Naveen P M <naveen.pm@moveinsync.com>
2023-10-05 18:57:41 +0530

    Added ultimate clock android project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
+    // To implement navigation's in compose
     implementation("androidx.navigation:navigation-compose:2.7.3")
 
 }
